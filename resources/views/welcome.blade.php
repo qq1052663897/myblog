ba14b22886dd3963fb47db6be96cfea0a3180177
zhangtengfei <1052663897@qq.com>
2021-09-18 17:18:19 +0800

    '提交编译后css文件'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,48 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <title>Document</title>
+    <link rel="shortcut icon" href="{{ asset('catty.png') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+    <title>My little space ~ </title>
 </head>
 <body>
-    <header class="max-w-lg mx-auto">
-        <a href="#">
-            <h1 class="text-4xl font-bold text-white text-center">Startup</h1>
-        </a>
-    </header>
-
-    <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-        <section>
-            <h3 class="font-bold text-2xl">Welcome to Startup</h3>
-            <p class="text-gray-600 pt-2">Sign in to your account.</p>
-        </section>
-
-        <section class="mt-10">
-            <form class="flex flex-col" method="POST" action="#">
-                <div class="mb-6 pt-3 rounded bg-gray-200">
-                    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Email</label>
-                    <input type="text" id="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
-                </div>
-                <div class="mb-6 pt-3 rounded bg-gray-200">
-                    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Password</label>
-                    <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
-                </div>
-                <div class="flex justify-end">
-                    <a href="#" class="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">Forgot your password?</a>
-                </div>
-                <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Sign In</button>
-            </form>
-        </section>
-    </main>
-
-    <div class="max-w-lg mx-auto text-center mt-12 mb-6">
-        <p class="text-white">Don't have an account? <a href="#" class="font-bold hover:underline">Sign up</a>.</p>
+    <div class="container max-auto px-4 origin-center">
+        <div class="w-24 h-24 origin-center">111</div>
     </div>
-
-    <footer class="max-w-lg mx-auto flex justify-center text-white">
-        <a href="#" class="hover:underline">Contact</a>
-        <span class="mx-3">•</span>
-        <a href="#" class="hover:underline">Privacy</a>
-    </footer>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container:{
+        center: true,
+    },
     extend: {},
   },
   variants: {
